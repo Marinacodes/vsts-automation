320133c86c284c3b9f8946c436c0d32586932478
Marina Retseptor <mretseptor@latitudegeo.com>
2017-04-20 18:03:24 -0700

    Initial commit

---
+++ AutomateVSTSTasks/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+
+namespace AutomateVSTSTasks
+{
+    public class Program
+    {
+        public static string domainName;
+        public static string personalAccessToken;
+        public static string credentials;
+        static ConsoleKeyInfo key;
+
+        public static string newProjectName;
+        public static string newProjectDescr;
+
+        public static string existingTeam;
+        public static string existingProject;
+        public static string newTeamName;
+        public static string newTeamDescr;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the domain you'd like to connect to and press Enter:");
+            Console.WriteLine("(https://{domainname}.visualstudio.com)");
+            domainName = Console.ReadLine();
+            Console.WriteLine("Please enter your personal access token and press Enter:");
+            MaskToken();
+            credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
+            
+            Console.WriteLine("What would you like to do? Type in the appropriate letter and press Enter.");
+          
+            Console.WriteLine("a. View Existing Team Projects");
+            Console.WriteLine("b. Create a New Team Project");
+            Console.WriteLine("c. View Existing Teams");
+            Console.WriteLine("d. Create a New Team");
+            Console.WriteLine("e. Get a Team's Iterations");
+            Console.WriteLine("f. Add an Iteration to a Team");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "a":
+                    Console.WriteLine("You typed 'a'");
+                    TaskGetTeamProjects();
+                    break;
+                case "b":
+                    Console.WriteLine("You typed 'b'");
+                    Console.WriteLine("Please provide a name for the new project and press Enter");
+                    newProjectName = Console.ReadLine();
+                    Console.WriteLine("Please provide a description for the new project and press Enter");
+                    newProjectDescr = Console.ReadLine();
+                    TaskCreateTeamProject();
+                    break;
+                case "c":
+                    Console.WriteLine("You typed 'c'");
+                    Console.WriteLine("Please provide a name for the existing project and press Enter");
+                    existingProject = Console.ReadLine();
+                    TaskGetTeams();
+                    break;
+                case "d":
+                    Console.WriteLine("You typed 'd'");
+                    Console.WriteLine("Please provide a name of the project you want to create the team in");
+                    existingProject = Console.ReadLine();
+                    Console.WriteLine("Please provide a name for the new team and press Enter");
+                    newTeamName = Console.ReadLine();
+                    Console.WriteLine("Please provide a description for the new team and press Enter");
+                    newTeamDescr = Console.ReadLine();
+                    TaskCreateTeam();
+                    break;
+                case "e":
+                    Console.WriteLine("You typed 'e'");
+                    Console.WriteLine("Please provide a name of the project");
+                    existingProject = Console.ReadLine();
+                    Console.WriteLine("Please provide a name of the team");
+                    existingTeam = Console.ReadLine();
+                    TaskGetIterations();
+                    break;
+                case "f":
+                    Console.WriteLine("You typed 'f'");
+                    Console.WriteLine("Please provide a name of the project");
+                    existingProject = Console.ReadLine();
+                    Console.WriteLine("Please provide a name of the team");
+                    existingTeam = Console.ReadLine();
+                    TaskAddIterations();
+                    break;
+            }
+            
+        }
+
+        static void MaskToken()
+        {
+            do
+            {
+                key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    personalAccessToken += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && personalAccessToken.Length > 0)
+                    {
+                        personalAccessToken = personalAccessToken.Substring(0, (personalAccessToken.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+        }
+
+        static void TaskGetTeamProjects()
+        {
+            var temp = new VSTSTasks();
+            temp.GetTeamProjects();
+        }
+
+        static void TaskCreateTeamProject()
+        {
+            var temp = new VSTSTasks();
+            temp.CreateTeamProject();
+        }
+
+        static void TaskCreateTeam()
+        {
+            var temp = new VSTSTasks();
+            temp.CreateTeam();
+        }
+
+        static void TaskGetTeams()
+        {
+            var temp = new VSTSTasks();
+            temp.GetTeams();
+        }
+
+        static void TaskGetIterations()
+        {
+            var temp = new VSTSTasks();
+            temp.GetIterations();
+        }
+
+        static void TaskAddIterations()
+        {
+            var temp = new VSTSTasks();
+            temp.AddIterations();
+        }
+    }
+}
---
+++ AutomateVSTSTasks/VSTSTasks.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AutomateVSTSTasks
+{
+    class VSTSTasks
+    {        
+        public void GetTeamProjects()
+        {    
+            //use the httpclient        
+            using (var client = new HttpClient())
+            {
+                //set our headers
+                client.BaseAddress = new Uri("https://" + Program.domainName + ".visualstudio.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                //send the request and content
+                HttpResponseMessage response = client.GetAsync("_apis/projects?api-version=2.2").Result;   
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    JObject responseJSON = JObject.Parse(result);
+                    int numProjects = (int)responseJSON["count"];
+                    Console.WriteLine("These are the current projects:");
+                    for (int i = 0; i < numProjects; i++)
+                    {
+                        Console.WriteLine((string)responseJSON["value"][i]["name"]);
+                    }
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public void CreateTeamProject()
+        {
+            //object to create a new team project
+            Object teamProject = new
+            {
+                name = Program.newProjectName,
+                description = Program.newProjectDescr,
+                capabilities = new
+                {
+                    versioncontrol = new
+                    {
+                        sourceControlType = "Git"
+                    },
+                    processTemplate = new
+                    {
+                        // Agile template type 
+                        // List here: https://www.visualstudio.com/en-us/docs/integrate/api/tfs/processes#getalistofprocesses
+                        templateTypeId = "adcc42ab-9882-485e-a3ed-7678f01f66bc"
+                    }
+                }
+            };
+
+            //use the httpclient
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                // serialize the fields array into a json string
+                var patchValue = new StringContent(JsonConvert.SerializeObject(teamProject), Encoding.UTF8, "application/json");
+                var method = new HttpMethod("POST");
+
+                var request = new HttpRequestMessage(method, "https://" + Program.domainName +".visualstudio.com/_apis/projects?api-version=2.2") { Content = patchValue };
+                var response = client.SendAsync(request).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+
+                }
+            }
+        }
+
+        public void CreateTeam()
+        {
+            //create a team object to save
+            Object teamData = new
+            {
+                name = Program.newTeamName,
+                description = Program.newTeamDescr,
+            };
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                // serialize the fields array into a json string         
+                var patchValue = new StringContent(JsonConvert.SerializeObject(teamData), Encoding.UTF8, "application/json"); // mediaType needs to be application/json-patch+json for a patch call
+                var method = new HttpMethod("POST");
+
+                var request = new HttpRequestMessage(method, "https://" + Program.domainName + ".visualstudio.com/_apis/projects/" + Program.existingProject + "/teams?api-version=2.2") { Content = patchValue };
+                var response = client.SendAsync(request).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public void GetTeams()
+        {
+            //use the httpclient        
+            using (var client = new HttpClient())
+            {
+                //set our headers
+                client.BaseAddress = new Uri("https://" + Program.domainName + ".visualstudio.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                //send the request and content
+                HttpResponseMessage response = client.GetAsync("_apis/projects/" + Program.existingProject + "/teams?api-version=2.2").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    JObject responseJSON = JObject.Parse(result);
+                    int numProjects = (int)responseJSON["count"];
+                    Console.WriteLine("These are the current teams:");
+                    for (int i = 0; i < numProjects; i++)
+                    {
+                        Console.WriteLine((string)responseJSON["value"][i]["name"]);
+                    }
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public void GetIterations()
+        {
+            //use the httpclient        
+            using (var client = new HttpClient())
+            {
+                //set our headers
+                client.BaseAddress = new Uri("https://" + Program.domainName + ".visualstudio.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                //send the request and content
+                HttpResponseMessage response = client.GetAsync(Program.existingProject + "/" +  Program.existingTeam + "/_apis/work/teamsettings/iterations?api-version=2.2").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    JObject responseJSON = JObject.Parse(result);
+                    int numProjects = (int)responseJSON["count"];
+                    Console.WriteLine("These are the current iterations for team: " + Program.existingTeam);
+                    for (int i = 0; i < numProjects; i++)
+                    {
+                        Console.WriteLine((string)responseJSON["value"][i]["name"] + 
+                            " " + (string)responseJSON["value"][i]["attributes"]["startDate"] + 
+                            " " + (string)responseJSON["value"][i]["attributes"]["finishDate"]);
+                    }
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        // This method is not working
+        public void AddIterations()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Program.credentials);
+
+                var method = new HttpMethod("POST");
+
+                var request = new HttpRequestMessage(method, "https://" + Program.domainName + ".visualstudio.com/" + Program.existingProject + "/" +
+                    Program.existingTeam + "/_apis/work/teamsettings/iterations?api-version=2.2");
+                var response = client.SendAsync(request).Result;
+                Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                Console.ReadLine();
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("\nReturned the following status code and description: " + (int)response.StatusCode + " " + response.StatusCode);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
